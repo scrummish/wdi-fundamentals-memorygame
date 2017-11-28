9a96110317b1fa0b58b0d633b950916a3baddd7c
Sergio Rocha <scrummish@gmail.com>
2017-11-28 11:45:46 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-let cards = ["queen","queen","king","king"];
+let cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 
 let cardsInPlay = [];
 
@@ -13,8 +34,10 @@ let checkForMatch = function (){
 }
 
 let flipCard = function (cardId){
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
     checkForMatch();
 }
 
